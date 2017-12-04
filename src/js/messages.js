865bb467b524e075f72bcacb5da1b1cbb8812bde
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2017-12-04 17:50:01 +0100

    added es6 and babel

---
+++ src/js/messages.js
@@ -0,0 +1,9 @@
+let messages = [
+    { id: 1, sender: 'Gabriel Mayta', subject: 'This is a test', description: '.......1' },
+    { id: 2, sender: 'Gabriel Mayta', subject: 'This is a test', description: '.......2' },
+    { id: 3, sender: 'Gabriel Mayta', subject: 'This is a test', description: '.......3' },
+    { id: 4, sender: 'Gabriel Mayta', subject: 'This is a test', description: '.......4' },
+    { id: 5, sender: 'Gabriel Mayta', subject: 'This is a test', description: '.......5' }
+];
+
+export default messages;
